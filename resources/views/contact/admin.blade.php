e45065ca23507c5ec6b3835c2737552b8c78cf94
edwardtua25 <107900532+edwardtua25@users.noreply.github.com>
2022-06-21 10:03:53 +0700

    Update admin.blade.php

--- resources/views/contact/admin.blade.php
+++ resources/views/contact/admin.blade.php
@@ -395,15 +395,20 @@
         </div>
     </div>
 </div>
-
-<div class="recent-grid">
+<div class="cards">
+    <div class="card-single">
+        <h1 id="galeri">Event</h1>
+        <span class="las la-email"></span>
+    </div></div>    
+   <div class="recent-grid">
     <div class="projects">
         <div class="card">
             <div class="card-header">
-                <h3 id="events">Even</h3>
+                <h3>List Event</h3>
 
-                <button><a href="/create-even" style="text-decoration: none;color:white;">Add Events </a> <span class="las la-user-plus">
+                <button><a href="/create-even" style="text-decoration: none;color:white;">Add Event</a> <span class="las la-user-plus">
 
+          
                 </span></button>
             </div>
             <div class="card-body"  id="costumer">
@@ -412,26 +417,27 @@
                         <tr>
                             <td>No</td>
                             <td>Gambar</td>
-                            <td>Nama Even</td>  
-                            <td>Action</td>                              
+                            <td>Deskripsi</td>  
+                            <td>Action</td>
+                                                     
                         </tr>
                     </thead>
-                    @foreach ($even as $time)
+                    @foreach ($even as $menu)
                     <tr>
-                        <td>{{ $no6++ }}</td>
-                        <td><img src="{{  url('asset/even/'.$time->gambar) }}" alt="" width="80px" height="80px"></td>
-                        <td>{{ $time->nama}}</td>        
+                        <td>{{ $no5++ }}</td>
+                        <td><img src="{{  asset('asset/even/'.$menu->gambar) }}" alt="" width="80px" height="80px"></td>
+                        <td>{{ $menu->deskripsi }}</td> 
                         <td>
-                            <a href="/edit-even/{{ $time->id }}" title="Edit Student"><button class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></a>
-                            <form method="POST" action="{{ url('/contact' . '/' . $time->id) }}" accept-charset="UTF-8" style="display:inline">
+                            <a href="/edit-even/{{ $menu->id }}" title="Edit Student"><button class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></a>
+                            <form method="POST" action="{{ url('/contact' . '/' . $menu->id) }}" accept-charset="UTF-8" style="display:inline">
                                 {{ method_field('DELETE') }}
                                 {{ csrf_field() }}
-                                <a href="/delete-even/{{$time->id}}" class="btn btn-danger btn-sm" title="Delete Contact" onclick="return confirm(&quot;Apakah data ini akan dihapus?&quot;)" style="margin: 10px"><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</a>
+                                <a href="/delete-even/{{$menu->id}}" class="btn btn-danger btn-sm" title="Delete Contact" onclick="return confirm(&quot;Apakah data ini akan dihapus?&quot;)" style="margin: 10px"><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</a>
                             </form>
-                        </td>                                                           
+                        </td>                                                             
                     </tr>
-
-                    @endforeach
+                    @endforeach {{ $even->links() }}
+                </table>
             </div>
         </div>
     </div>
