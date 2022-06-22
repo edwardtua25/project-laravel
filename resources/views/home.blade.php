6e20a6f400572360f5231582ab40e60265fb83df
edwardtua25 <107900532+edwardtua25@users.noreply.github.com>
2022-06-22 12:49:20 +0700

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -36,8 +36,8 @@
     <div class="card border-dark">
      <p class="text-center" style="font-family: 'Courier New', Courier, monospace; font-height:bold; font-size:15px;"><b>WELCOME TO THE El Tona CoffeeHouse </b><br>
       OPENING HOURS <br>
-      Monday to Friday 8am 22pm <br>
-      Saturday and Sunday 8am 24pm <br>
+      Monday to Friday 8am-22pm <br>
+      Saturday and Sunday 8am-24pm <br>
       Don’t forget to always update today’s menu
       El Tona CafeHouse merupakan salah satu café yang terletak di Jl. Horas No. 81, Tomok Parsaoran, Samosir. <br> Cafe ini mulai beroperasi 5 tahun tepatnya pada tanggal 10 Juni 2018. <br> El Tona CoffeeHouse memiliki 12 kariawan local yang mengutamakan tenaga <br> kerja daearah setempat dan di dukung dengan barista, jangan lupa mampir ya 😊. <br>
       Ingin menikmati makanan dengan santai dan hangat berdama teman dan pasangan? <br> Tentu bisa, Kami menyediakan Live Music setiap hari malam nya dan didukung dengan tema nuansa tropical flower <br> membuat makan bersama pasangan semakin romantis. <br>
