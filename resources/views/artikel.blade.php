cee2e100edce49ec95f712d8613530ea11e70fb3
edwardtua25 <107900532+edwardtua25@users.noreply.github.com>
2022-06-22 15:10:26 +0700

    Update artikel.blade.php

--- resources/views/artikel.blade.php
+++ resources/views/artikel.blade.php
@@ -41,7 +41,7 @@ Dengan adanya Menu Of The Day ini, kantong kamu bakal lebih irit! Yuk, buruan ga
               <div class="col-md-4">
                 <div class="card-body">
                    <h6 class="card-title" style="font-family: Verdana, Geneva, Tahoma, sans-serif">Judul:{{ $satu->nama }}</h6>
-                  <p style="background-color: antiquewhite;font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif">Time:{{ $satu->waktu }}</p>
+                  <p style="background-color: antiquewhite;font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif">Waktu:{{ $satu->waktu }}</p>
                   <p class="card-text" style="font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;letter-spacing:2px;">Artikel:<br>{{ $satu->deskripsi }}</p>
  
                 </div>            
